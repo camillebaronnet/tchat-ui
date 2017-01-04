32d69c95e266954f7ae6a6aea4be21642bb34ffe
Camille Baronnet <git@camillebaronnet.fr>
2017-01-04 20:34:32 +0100

    Create minified version

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,11 +2,23 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var minCss = require('gulp-minify-css');
+var rename = require('gulp-rename');
 
 gulp.task('sass', function () {
   return gulp.src('./scss/**/*.scss')
+
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./dist/css'));
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('./dist/css'))
+
+    // Minified version
+    .pipe(minCss())
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(gulp.dest('./dist/css'))
+
+    ;
 });
 
 gulp.task('sass:watch', function () {
